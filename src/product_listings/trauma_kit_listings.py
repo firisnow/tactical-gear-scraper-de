141a6e49aab3eb42ba35f89604e94b1a3bcea91e
tet <ge56pem@mytum.de>
2022-03-21 20:42:42 +0100

    telegram bot code added

--- src/product_listings/trauma_kit_listings.py
+++ src/product_listings/trauma_kit_listings.py
@@ -1,4 +1,5 @@
 trauma_kit_url_list = [
     "https://www.medididakt.de/shop/notfallkoffer-taschen/notfalltaschen/taktische-tasche-medical-bag-gefuellt/",
+    "https://www.ostalb-med-shop.de/EIFAK--IFAK--Polizei--Police--BW--Baden-Wuerttemberg-.html",
 
 ]
